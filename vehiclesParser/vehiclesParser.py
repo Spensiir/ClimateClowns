cd43422be613e9dcbfe587478cfa10698bdc05f0
mayalamdap <49964356+mayalamdap@users.noreply.github.com>
2020-04-19 00:12:54 -0400

    subgraphs and fuelcost

--- vehiclesParser/vehiclesParser.py
+++ vehiclesParser/vehiclesParser.py
@@ -4,29 +4,7 @@ from collections import defaultdict
 vehicles = pd.read_csv("vehiclesdataset.csv",low_memory=False)
 emissions = pd.read_csv("emissions.csv")
 
-data = vehicles.merge(emissions, on="id")
+# data = vehicles.merge(emissions, on="id")
 
-mpgData = data['mpgData'] == 'Y'
-mpgData = data[mpgData]
-ghgScore = data['ghgScore'] == '-1'
-
-reg = data[ghgScore]
-# scoreData = reg['score'] != None
-# reg = reg[scoreData]
-# reg = reg.groupby(['year']).groups
-reg = reg.groupby(['fuelType','year','VClass', 'highway08','fuelCost08', 'score', 'ghgScore'])['city08'].mean().to_csv('output_file.csv')
-# regC = reg['year']('city08').mean()
-# regH = reg.groupby('year')['highway08'].mean()
-
-
-# prem = data[data['fuelType'] == 'Premium']
-# prem = prem[mpgData]
-# premC = prem.groupby('year')['city08'].mean()
-# premH = prem.groupby('year')['highway08'].mean()
-
-# dies = data[data['fuelType'] == 'Diesel']
-# nat = data[data['fuelType'] == 'CNG']
-print(reg)
-# print(regH)
-# print(premC)
-# print(premH)
+vehicles = vehicles[vehicles.mpgData == 'Y']
+vehicles = vehicles.groupby(['fuelType','year','VClass', 'highway08','fuelCost08'])['city08'].mean().to_csv('output_file.csv')
